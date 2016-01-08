b4b7348b99c9536b93a9c3cb1ad9c403b4452fd6
yuechong <zyc740321582@126.com>
2016-01-08 14:29:20 +0800

    add

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+var bookStoreApp = angular.module('bookStoreApp', [
+    'ngRoute', 'ngAnimate', 'bookStoreCtrls', 'bookStoreFilters',
+    'bookStoreServices', 'bookStoreDirectives'
+]);
+
+bookStoreApp.config(function($routeProvider) {
+    $routeProvider.when('/hello', {
+        templateUrl: 'tpls/hello.html',
+        controller: 'HelloCtrl'
+    }).when('/list',{
+    	templateUrl:'tpls/bookList.html',
+    	controller:'BookListCtrl'
+    }).when('/login',{
+	    templateUrl:'tpls/login.html',
+		controller:'LoginCtrl'
+	}).otherwise({
+        redirectTo: '/hello'
+    })
+});
